2e23bd6db3cd4f8b27f4399f46ac63d88ca0a3e8
PXgamer <owzie123@gmail.com>
2017-02-23 14:18:36 +0000

    PSR-2 fix

--- src/functions/Quick.php
+++ src/functions/Quick.php
@@ -1,26 +1,28 @@
 <?php
+
 namespace pxgamer\TorrentSlackBots\Functions;
 
 class Quick
 {
+    public static function mksize($s, $precision = 2)
+    {
+        $suf = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
 
-	public static function mksize ($s, $precision = 2) {
-		$suf = array("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+        for ($i = 1, $x = 0; $i <= count($suf); $i++, $x++) {
+            if ($s < pow(1024, $i) || $i == count($suf)) { // Change 1024 to 1000 if you want 0.98GB instead of 1,0000MB
+                return number_format($s / pow(1024, $x), $precision).' '.$suf[$x];
+            }
+        }
+    }
 
-		for ($i = 1, $x = 0; $i <= count($suf); $i++, $x++) {
-			if ($s < pow(1024, $i) || $i == count($suf)) // Change 1024 to 1000 if you want 0.98GB instead of 1,0000MB
-				return number_format($s/pow(1024, $x), $precision)." ".$suf[$x];
-		}
-	}
+    public static function parseTurl($url)
+    {
+        $url = explode('/', $url);
 
-	public static function parseTurl($url) {
-		$url = explode('/', $url);
-		
-		return (object)[
-			"id" => $url[4],
-			"url" => "https://extra.to/torrent/" . $url[4],
-			"dl" => "https://extra.to/download/" . $url[4]
-		];
-	}
-
+        return (object) [
+            'id' => $url[4],
+            'url' => 'https://extra.to/torrent/'.$url[4],
+            'dl' => 'https://extra.to/download/'.$url[4],
+        ];
+    }
 }
